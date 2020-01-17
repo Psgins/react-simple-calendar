009ec8c77421ead49b4a4d16fd87eb93a1004475
Peerachat Uengpattanakit <peerachat@gmail.com>
2020-01-17 10:42:21 +0700

    Added form for dialog

--- src/components/CalDialog.tsx
+++ src/components/CalDialog.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useState, FormEvent } from 'react';
 import { format } from 'date-fns';
 import Dialog from '@material-ui/core/Dialog';
 import { DialogTitle, DialogContent, TextField, DialogActions, Button } from '@material-ui/core';
@@ -14,30 +14,34 @@ const CalDialog: FC<CalDialogProps> = ({ open, date, description, onSave, onCanc
   const [inputDescription, setInputDescription] = useState(description || '');
 
   const handleOnCancel = () => {
+    setInputDescription(description || '');
     onCancel();
   };
 
-  const handleOnSave = () => {
+  const handleOnSave = (e: FormEvent) => {
+    e.preventDefault();
     onSave(inputDescription);
   };
 
   return (
     <Dialog fullWidth open={open} maxWidth="sm">
-      <DialogTitle>{format(date, 'd MMMM yyyy')}</DialogTitle>
-      <DialogContent>
-        <TextField
-          fullWidth
-          label="Description"
-          value={inputDescription}
-          onChange={e => setInputDescription(e.target.value.substr(0, 40))}
-        />
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleOnCancel}>Cancel</Button>
-        <Button color="primary" variant="contained" onClick={handleOnSave}>
-          Save
-        </Button>
-      </DialogActions>
+      <form onSubmit={handleOnSave}>
+        <DialogTitle>{format(date, 'd MMMM yyyy')}</DialogTitle>
+        <DialogContent>
+          <TextField
+            fullWidth
+            label="Description"
+            value={inputDescription}
+            onChange={e => setInputDescription(e.target.value.substr(0, 40))}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleOnCancel}>Cancel</Button>
+          <Button type="submit" color="primary">
+            Save
+          </Button>
+        </DialogActions>
+      </form>
     </Dialog>
   );
 };
